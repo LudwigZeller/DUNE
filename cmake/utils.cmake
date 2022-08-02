6ab133efb4f0a109f9a247376809c42bd7b9c58d
Ludwig Zeller <91737401+LudwigZeller@users.noreply.github.com>
2022-08-02 14:17:19 +0200

    Corrected Error in CMakeLists.txt

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -1,12 +1,14 @@
+# Only works with relative path
 function(copy_files)
     foreach (ARG ${ARGV})
         copy_file(${ARG})
     endforeach ()
 endfunction()
 
+# Only works with relative path
 function(copy_file FILE)
-    add_custom_command(TARGET ${PROJECT_NAME} POST_BUILD
-            COMMAND ${CMAKE_COMMAND} -E copy_if_different
-            "${FILE}"
-            $<TARGET_FILE_DIR:${PROJECT_NAME}>)
+    add_custom_command(TARGET ${PROJECT_NAME} POST_BUILD        # Adds a post-build event to MyTest
+            COMMAND ${CMAKE_COMMAND} -E copy_if_different  # which executes "cmake - E copy_if_different..."
+            "${PROJECT_SOURCE_DIR}/${FILE}"      # <--this is in-file
+            $<TARGET_FILE_DIR:${PROJECT_NAME}>)                 # <--this is out-file path
 endfunction()
