be2cc6873cc28a1275396b11527d6bbf7c393ad8
Nievillis <dgtroll.schoosi@gmail.com>
2022-11-18 09:13:10 +0100

    Added universal generation tools

--- 80_DEV/cmake/utils.cmake
+++ 80_DEV/cmake/utils.cmake
@@ -50,10 +50,10 @@ function(copy_directory_dll DIR)
 endfunction()
 
 # MakeDLogo script
-function(makeasset DEP WD NAM)
+function(makeasset DEP WD NAM PNGNAM SRC)
 #    add_custom_target(DuneLogoMaker COMMAND bash ${PROJECT_SOURCE_DIR}/assets/MakeDLogo.bash ${PROJECT_SOURCE_DIR}/assets)
     add_custom_target(${DEP}
-        COMMAND make run name=${NAM}
+        COMMAND make run name=${NAM} outputname=${NAM} targetpng=${PNGNAM} SOURCES=${SRC}
         WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}/assets/${WD}
     )
     add_dependencies(${PROJECT_NAME} ${DEP})
